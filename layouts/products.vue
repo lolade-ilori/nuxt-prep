978d045ba9acc12c8c49739d012b10b4c903d2dd
Lolade Ilori <loladeilori02@gmail.com>
2025-02-17 05:00:25 +0000

    first commit

---
+++ layouts/products.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <header>
+            <nav>
+                <NuxtLink to="/">Nuxt Prep</NuxtLink>
+            </nav>
+        </header>
+
+        <div>
+            <slot />
+        </div>
+
+        <footer>
+            <ul>
+                    <li><NuxtLink to="/">Home</NuxtLink></li>
+                    <li><NuxtLink to="/about">About</NuxtLink></li>
+                    <li><NuxtLink to="/products">Products</NuxtLink></li>
+                </ul>
+        </footer>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style  scoped>
+.router-link-active, .router-link-exact-active {
+    color: green;
+}
+</style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h2>This is for Product {{ id }}</h2>
+
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias, odit consequatur deleniti ratione cupiditate quos libero recusandae pariatur, sit quas corrupti. Quam doloribus similique obcaecati rerum recusandae dolor laboriosam porro.</p>
+    </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params
+
+definePageMeta({
+    layout: 'products'
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
