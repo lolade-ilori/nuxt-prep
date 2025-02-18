46d3b9652fcd2d5bee03251c6b939ea003e6d8b3
Lolade Ilori <loladeilori02@gmail.com>
2025-02-18 15:09:00 +0000

    error page from server and browser

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -9,8 +9,11 @@ const uri = "https://fakestoreapi.com/products/" + id
 definePageMeta({
     layout: 'products'
 })
-
 const { data:product } = await useFetch(uri, {key: id})
+
+if(!product.value) {
+    throw createError({statusCode: 404, statusMessage: 'Product not found', fatal: true})
+}
 </script>
 
 <style lang="scss" scoped>
